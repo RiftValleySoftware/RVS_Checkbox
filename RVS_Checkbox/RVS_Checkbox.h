c94b005ed71562e958532c803539e8bb2c9f19cf
Chris Marshall <chris@riftvalleysoftware.com>
2021-03-15 08:55:32 -0400

    Initial Commit

---
+++ RVS_Checkbox/RVS_Checkbox.h
@@ -0,0 +1,18 @@
+//
+//  RVS_Checkbox.h
+//  RVS_Checkbox
+//
+//  Created by Chris Marshall on 3/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RVS_Checkbox.
+FOUNDATION_EXPORT double RVS_CheckboxVersionNumber;
+
+//! Project version string for RVS_Checkbox.
+FOUNDATION_EXPORT const unsigned char RVS_CheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RVS_Checkbox/PublicHeader.h>
+
+
