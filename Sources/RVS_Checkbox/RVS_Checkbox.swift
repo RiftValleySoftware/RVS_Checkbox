0642299a8426ec31ad99fed1446ff21b5d07d537
Chris Marshall <chris@riftvalleysoftware.com>
2021-03-20 19:15:19 -0400

    Added the version to the source file.

--- Sources/RVS_Checkbox/RVS_Checkbox.swift
+++ Sources/RVS_Checkbox/RVS_Checkbox.swift
@@ -18,6 +18,8 @@
  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
  The Great Rift Valley Software Company: https://riftvalleysoftware.com
+ 
+ Version 1.1.0
 */
 
 import UIKit
